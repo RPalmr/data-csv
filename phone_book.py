917b271b087e36929b740ec86c6980e8a21710f4
reece palmer <reecelpalmer@gmail.com>
2023-10-18 09:58:39 +0100

    Progress on 1 tests passing

--- phone_book.py
+++ phone_book.py
@@ -1,1 +1,13 @@
+import csv
 
+with open('data/phone_book.csv', mode='r') as csv_file:
+    csv_reader = csv.reader(csv_file)
+    line_count = 0
+    for row in csv_reader:
+        if line_count == 0:
+            """skip header"""
+            line_count += 1
+        else:
+            last_name, phone_number = row[1], row[2]
+            print(f'{last_name}: {phone_number}')
+            line_count += 1
