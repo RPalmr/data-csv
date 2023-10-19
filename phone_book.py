7a05a4eb4089eaef272bef25e9159f1fcdb5ae49
reece palmer <reecelpalmer@gmail.com>
2023-10-19 16:09:57 +0100

    update style score

--- phone_book.py
+++ phone_book.py
@@ -1,20 +1,5 @@
 import csv
 
-"""original code"""
-# import csv
-
-# with open('data/phone_book.csv', mode='r') as csv_file:
-    # csv_reader = csv.reader(csv_file)
-    # line_count = 0
-    # for row in csv_reader:
-        # if line_count == 0:
-            # Skip the header
-            # line_count += 1
-        # else:
-            # last_name, phone_number = row[1], row[2]
-            # print(f'{last_name}: {phone_number}')
-            # line_count += 1
-
 with open('data/phone_book.csv', mode='r') as csv_file:
     """csv dict implemented here"""
     csv_reader = csv.DictReader(csv_file)
